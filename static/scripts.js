034745b415f85f49d3d68017b9f0d039a1dcdd9a
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2015-03-03 11:40:05 -0300

    Improve slider responsiveness.

--- static/scripts.js
+++ static/scripts.js
@@ -8,7 +8,7 @@ $('.js-menu-trigger').click(function() {
 
 
 // rotating news
-$(".js-news-wrapper > h5:gt(0)").hide();
+$('.js-news-wrapper > h5:gt(0)').hide();
 
 setInterval(function() {
 	$('.js-news-wrapper > h5:first').fadeOut(1000, function() {
@@ -22,32 +22,15 @@ setInterval(function() {
 
 // Products Slider - http://unslider.com/
 var slider = $('.b-slider').unslider({
-	speed: 500,               //  The speed to animate each slide (in milliseconds)
-	delay: 3000,              //  The delay between slide animations (in milliseconds)
-	// complete: function() {},  //  A function that gets called after every slide animation
-	// keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-	// dots: true,               //  Display dot navigation
-	fluid: false              //  Support responsive design. May break non-responsive designs
+	keys: false,
+	dots: false,
+	fluid: true
 });
-
-var data = slider.data('unslider');
-
-slider.hover(
-	function() {
-		//  Pause Unslider
-		data.stop();
-		console.log("Stopped");
-	}, function() {
-		//  Start Unslider
-		data.start();
-		console.log("Moving Again");
-	}
-);
 
 // Smooth Scrolling Function
 $(function() {
 	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
 			|| location.hostname == this.hostname) {
 
 		var target = $(this.hash);
@@ -61,8 +44,5 @@ $(function() {
 		}
 	});
 });
-
-
-
 
 }); // doc.ready
