55bd91086d1ad7988f7d3dea9ebdb4377471510a
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2015-03-03 10:54:21 -0300

    Fix fading speed of News section.

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,6 @@
 $(document).foundation();
 $(document).ready(function() {
-		
+
 // trigger for the main menu mobile
 $('.js-menu-trigger').click(function() {
 	$('.b-main-menu').toggleClass('m-active');
@@ -10,19 +10,17 @@ $('.js-menu-trigger').click(function() {
 // rotating news
 $(".js-news-wrapper > h5:gt(0)").hide();
 
-setInterval(function() { 
-  $('.js-news-wrapper > h5:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('.js-news-wrapper');
-},  6000);
+setInterval(function() {
+	$('.js-news-wrapper > h5:first').fadeOut(1000, function() {
+		$(this).next().fadeIn(1000);
+		$(this).appendTo('.js-news-wrapper');
+	});
+}, 6000);
 
 
 
 
-// Products Slider - http://unslider.com/ 
+// Products Slider - http://unslider.com/
 var slider = $('.b-slider').unslider({
 	speed: 500,               //  The speed to animate each slide (in milliseconds)
 	delay: 3000,              //  The delay between slide animations (in milliseconds)
@@ -38,11 +36,11 @@ slider.hover(
 	function() {
 		//  Pause Unslider
 		data.stop();
-		console.log("Stopped");	
+		console.log("Stopped");
 	}, function() {
 		//  Start Unslider
 		data.start();
-		console.log("Moving Again");	
+		console.log("Moving Again");
 	}
 );
 
