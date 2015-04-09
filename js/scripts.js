b4ad9d3997a78ad11d0e45492e4ab82fc31d55c9
James McKinney <james@slashpoundbang.com>
2015-04-09 00:01:57 -0400

    index with spaces

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
 
 // trigger for the main menu mobile
 $('.js-menu-trigger').click(function() {
-	$('.b-main-menu').toggleClass('m-active');
+  $('.b-main-menu').toggleClass('m-active');
 });
 
 // rotating news
 $('.js-news-wrapper > h5:gt(0)').hide();
 
 setInterval(function() {
-	$('.js-news-wrapper > h5:first').fadeOut(1000, function() {
-		$(this).next().fadeIn(1000);
-		$(this).appendTo('.js-news-wrapper');
-	});
+  $('.js-news-wrapper > h5:first').fadeOut(1000, function() {
+    $(this).next().fadeIn(1000);
+    $(this).appendTo('.js-news-wrapper');
+  });
 }, 6000);
 
 // Background switcher.
@@ -28,28 +28,28 @@ setInterval(function() {
 
 // Products Slider - http://unslider.com/
 // var slider = $('.b-slider').unslider({
-// 	dots: true,
-// 	keys: false,
-// 	fluid: true
+//   dots: true,
+//   keys: false,
+//   fluid: true
 // });
 
 
 // Smooth Scrolling Function
 $(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-			|| location.hostname == this.hostname) {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname) {
 
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		if (target.length) {
-			$('html,body').animate({
-			  scrollTop: target.offset().top
-			}, 1000);
-			return false;
-			}
-		}
-	});
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+      }
+    }
+  });
 });
 
 }); // doc.ready
